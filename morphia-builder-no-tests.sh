f3b9511a4c6ffd54bf38356cb569fa341653a352
Maxim <maksukh@inbox.ru>
2021-09-20 07:38:43 +0300

    Implement morphia builder

---
+++ morphia-builder-no-tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Make sure JDK version is 11 and maven is installed.
+
+GIT_REPO=https://github.com/MorphiaOrg/morphia
+NUM_COMMITS_TO_BUILD=100
+
+git clone $GIT_REPO -q
+rm -rf errors_log_morphia.txt
+cd morphia
+
+for ((i=$NUM_COMMITS_TO_BUILD-1; i>=0; i--))
+do
+    info=`git checkout master~$i 2>&1 | sed -n "s/HEAD is now at /$i. /gp"`
+    echo "$info"
+    out=`mvn install -DskipTests 2>&1`
+    if grep -q "BUILD SUCCESS" <<< "$out"; then
+        echo "BUILD OK"
+    else
+        echo "BUILD ERROR (for details see errors_log_morphia.txt)"
+        echo "$info" >>../errors_log_morphia.txt
+        echo "$out" >>../errors_log_morphia.txt
+        echo "" >>../errors_log_morphia.txt
+    fi
+done
+
+cd ..
+rm -rf morphia
